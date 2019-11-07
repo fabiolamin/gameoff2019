e5d815cbc70eb697a332d3a89477e4c6ff8f72bd
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-07 09:16:19 -0300

    Creating EnemySpawn script

---
+++ gameoff2019/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    private float timerAux;
+    [SerializeField]
+    private float spawnInterval = 1f;
+    [SerializeField]
+    private GameObject[] enemies;
+    [SerializeField]
+
+    public float SpawnInterval
+    {
+        get { return spawnInterval; }
+        set { spawnInterval = value; }
+    }
+
+    private void Awake()
+    {
+        timerAux = spawnInterval;
+    }
+    private void Update()
+    {
+        timerAux -= Time.deltaTime;
+        if (timerAux < 0)
+        {
+            Spawn();
+            timerAux = spawnInterval;
+        }
+    }
+    void Spawn()
+    {
+        int random = Random.Range(0, enemies.Length);
+        Instantiate(enemies[random], transform.position, Quaternion.identity);
+    }
+}
