a295bd92998516dd051a849d05eb326138626cdf
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-10 09:30:49 -0300

    Creating AttackZone script

---
+++ gameoff2019/Assets/Scripts/Attack Zone/AttackZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackZone : MonoBehaviour
+{
+    public bool isTargetInside { get; private set; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            isTargetInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            isTargetInside = false;
+        }
+    }
+}
