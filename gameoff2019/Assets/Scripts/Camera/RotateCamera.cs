5820b56d9e845e056112a1f4bb7cd930084a7473
Rodolfo <rodolfomsouza@gmail.com>
2019-11-13 16:14:02 -0300

    Rotate Camera Script

---
+++ gameoff2019/Assets/Scripts/Camera/RotateCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * 150);
+            //offset++;
+            //transform.localEulerAngles = new Vector3(0, transform.eulerAngles.y + offset *Time.deltaTime, 0);
+        }
+    }
+}
