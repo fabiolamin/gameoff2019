2c77541fa9851794ebb5dd811661b911e06fb601
Rodolfo <rodolfomsouza@gmail.com>
2019-11-14 17:29:19 -0300

    Selecting the base of the towers

---
+++ gameoff2019/Assets/ConfigureBaseTowers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigureBaseTowers : MonoBehaviour
+{
+    private GameObject[] bases;
+    private GameObject toolbarBaseTower;
+    private Animator animatorToolbarTower;
+    private float timeToolbarOpen = 0;
+    void Start()
+    {
+        PressetBaseTowers();
+    }
+
+    private void PressetBaseTowers()
+    {
+        toolbarBaseTower = GameObject.FindGameObjectWithTag("ToolbarBaseTower");
+        animatorToolbarTower = toolbarBaseTower.GetComponent<Animator>();
+        bases = GameObject.FindGameObjectsWithTag("BaseTower");
+        foreach (GameObject go in bases)
+        {
+            go.GetComponent<SelectBase>().SetToolbarBase(toolbarBaseTower);
+        }
+    }
+
+    void Update()
+    {
+        CheckTowerBaseAnimation();
+    }
+
+    private void CheckTowerBaseAnimation()
+    {
+        if (Time.time > timeToolbarOpen)
+        {
+            animatorToolbarTower.SetBool("isOpen", false);
+            animatorToolbarTower.SetBool("isClose", true);
+        }
+    }
+
+    public void AddTimeToolbarBase(int seconds)
+    {
+        timeToolbarOpen = Time.time + seconds;
+    }
+}
---
+++ gameoff2019/Assets/Scripts/Base Tower/SelectBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectBase : MonoBehaviour
+{
+    private GameObject toolbarBase;
+    private Animator animator;
+    private ConfigureBaseTowers cbt;
+
+    private void Start()
+    {
+        cbt = FindObjectOfType<ConfigureBaseTowers>();
+    }
+
+    public void SetToolbarBase(GameObject goToolbarBaseTower)
+    {
+        toolbarBase = goToolbarBaseTower;
+        animator = toolbarBase.GetComponent<Animator>();
+    }
+    private void OnMouseDown()
+    {
+        animator.SetBool("isOpen", true);
+        animator.SetBool("isClose", false);
+        cbt.AddTimeToolbarBase(3);
+    }
+}
