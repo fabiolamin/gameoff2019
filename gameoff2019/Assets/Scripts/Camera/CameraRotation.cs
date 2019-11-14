68a9124a3e3b25ffcc35b82c7de9a55473a71a27
Rodolfo <rodolfomsouza@gmail.com>
2019-11-14 13:14:51 -0300

    Updating RotateCamera name class for CameraRotation

---
+++ gameoff2019/Assets/Scripts/Camera/CameraRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * 150);
+        }
+    }
+}
