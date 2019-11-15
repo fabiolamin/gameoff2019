423a47b5e4ea8806170b04b33c6d851deba4a72b
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-15 09:26:42 -0300

    Adjusting EnemyMovement script

--- gameoff2019/Assets/Scripts/Enemy/EnemyMovement.cs
+++ gameoff2019/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,14 +5,14 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    private float speed = 3f;
     private NavMeshAgent enemyNavMeshAgent;
     [SerializeField]
-    private float speed = 3f;
     Transform target;
     public float Speed
     {
         get { return speed; }
-        set { speed = value; }
+        private set { speed = value; }
     }
 
     private void Awake()
@@ -21,14 +21,13 @@ public class EnemyMovement : MonoBehaviour
         enemyNavMeshAgent.speed = speed;
     }
 
+    private void Update()
+    {
+        Move();
+    }
+
     void Move()
     {
         enemyNavMeshAgent.destination = target.position;
-    }
-
-    public void SetTarget(Transform transformTarget)
-    {
-        target = transformTarget;
-        Move();
     }
 }
