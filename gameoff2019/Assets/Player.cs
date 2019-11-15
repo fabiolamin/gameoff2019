0c125856f785325476f8deb810988992d0d7a17b
Rodolfo <rodolfomsouza@gmail.com>
2019-11-15 13:08:57 -0300

    Buy Tower

---
+++ gameoff2019/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int coins = 500;
+    [SerializeField]
+    private int life = 10;
+
+    public int Coins
+    {
+        get { return coins; }
+        set { coins = value; }
+    }
+
+    public int Life
+    {
+        get { return life; }
+        set { life = value; }
+    }
+}
--- gameoff2019/Assets/Scripts/Tower/ChangeTowers.cs
+++ gameoff2019/Assets/Scripts/Tower/ChangeTowers.cs
@@ -6,9 +6,24 @@ public class ChangeTowers : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] towers;
+    [SerializeField]
+    private int[] price;
 
     public void ActiveTower(int value)
     {
-        towers[value].SetActive(true);
+        Player player = FindObjectOfType<Player>();
+        int wallet = player.Coins;
+        if (wallet >= price[value])
+        {
+            wallet -= price[value];
+            player.Coins = wallet;
+            towers[value].SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Insufficient Coins");
+        }
+        
+        
     }
 }
