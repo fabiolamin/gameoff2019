756e7718bdd094f065d96f32d1ce98462bf9cbae
Rodolfo <rodolfomsouza@gmail.com>
2019-11-16 12:33:30 -0300

    Scene Main

---
+++ gameoff2019/Assets/Scripts/Load Game/LoadGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+    int indexScene;
+
+    private void Start()
+    {
+        indexScene = SceneManager.GetActiveScene().buildIndex;
+    }
+    public void Play()
+    {
+        PressetGame();
+        SceneManager.LoadScene(indexScene + 1);
+    }
+
+    private static void PressetGame()
+    {
+        PlayerPrefs.SetInt("Coins", 0);
+
+        PlayerPrefs.SetInt("Stage1", 1);
+        PlayerPrefs.SetInt("Stage2", 0);
+        PlayerPrefs.SetInt("Stage3", 0);
+        PlayerPrefs.SetInt("Stage4", 0);
+        PlayerPrefs.SetInt("Stage5", 0);
+        PlayerPrefs.SetInt("Stage6", 0);
+        PlayerPrefs.SetInt("Stage7", 0);
+        PlayerPrefs.SetInt("Stage8", 0);
+        PlayerPrefs.SetInt("Stage9", 0);
+        PlayerPrefs.SetInt("Stage10", 0);
+
+        PlayerPrefs.SetInt("StartedGame", 1);
+    }
+
+    public void Continue()
+    {
+        int startedGame = PlayerPrefs.GetInt("StartedGame");
+        if (startedGame > 0)
+        {
+            SceneManager.LoadScene(indexScene + 1);
+        }
+    }
+}
