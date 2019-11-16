47239db9eadf6046ae2f1d566fb15f223df5b300
Rodolfo <rodolfomsouza@gmail.com>
2019-11-16 14:04:17 -0300

    Load Stage

---
+++ gameoff2019/Assets/LoadLevel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField]
+    private int stageIndexScene;
+
+    private void OnMouseDown()
+    {
+        switch (stageIndexScene)
+        {
+            case 2:
+                LoadStage("Stage1", stageIndexScene);
+                break;
+            case 3:
+                LoadStage("Stage2", stageIndexScene);
+                break;
+            case 5:
+                LoadStage("Stage3", stageIndexScene);
+                break;
+            case 6:
+                LoadStage("Stage4", stageIndexScene);
+                break;
+            case 7:
+                LoadStage("Stage5", stageIndexScene);
+                break;
+            case 8:
+                LoadStage("Stage6", stageIndexScene);
+                break;
+            case 9:
+                LoadStage("Stage7", stageIndexScene);
+                break;
+            case 10:
+                LoadStage("Stage8", stageIndexScene);
+                break;
+            case 11:
+                LoadStage("Stage9", stageIndexScene);
+                break;
+            case 12:
+                LoadStage("Stage10", stageIndexScene);
+                break;
+        }
+    }
+
+    private static void LoadStage(string stage, int sceneIndex)
+    {
+        if (PlayerPrefs.GetInt(stage) > 0)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
--- gameoff2019/Assets/Scripts/Preset World/PresetWorld.cs
+++ gameoff2019/Assets/Scripts/Preset World/PresetWorld.cs
@@ -7,6 +7,8 @@ public class PresetWorld : MonoBehaviour
 {
     [SerializeField]
     Text textCoins;
+    [SerializeField]
+    GameObject[] stages;
     int[] qualifiedStage;
     int coins;
     // Start is called before the first frame update
@@ -14,7 +16,7 @@ public class PresetWorld : MonoBehaviour
     {
         qualifiedStage = new int[11] {
             0,
-            PlayerPrefs.GetInt("Stage1"), 
+            PlayerPrefs.GetInt("Stage1"),
             PlayerPrefs.GetInt("Stage2"),
             PlayerPrefs.GetInt("Stage3"),
             PlayerPrefs.GetInt("Stage4"),
@@ -24,6 +26,13 @@ public class PresetWorld : MonoBehaviour
             PlayerPrefs.GetInt("Stage8"),
             PlayerPrefs.GetInt("Stage9"),
             PlayerPrefs.GetInt("Stage10")};
+
+        for (int i = 1; i < qualifiedStage.Length; i++)
+        {
+            bool isActive = qualifiedStage[i] != 0;
+            Debug.Log(isActive);
+            stages[i].SetActive(isActive);
+        }
 
         coins = PlayerPrefs.GetInt("Coins");
         textCoins.text = coins.ToString();
