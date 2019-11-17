18eaa690f32d5b317a3f2809cd1577b110a2a8ee
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-17 11:34:22 -0300

    Creating Pool script

---
+++ gameoff2019/Assets/Scripts/Pool/Pool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    private GameObject[] InstantiatePrefabs;
+
+    private void Awake()
+    {
+        InstantiatePrefabs = new GameObject[60];
+        SetPrefabs();
+    }
+
+    private void SetPrefabs()
+    {
+        for (int x = 0; x < InstantiatePrefabs.Length; x++)
+        {
+            GameObject newPrefab = Instantiate(prefab, transform.position, Quaternion.identity);
+            newPrefab.SetActive(false);
+            InstantiatePrefabs[x] = newPrefab;
+        }
+    }
+
+    public GameObject GetPrefabs()
+    {
+        for (int x = 0; x < InstantiatePrefabs.Length; x++)
+        {
+            GameObject prefab = InstantiatePrefabs[x];
+            prefab.SetActive(true);
+            return prefab;
+        }
+
+        RecyclePrefabs();
+        return null;
+    }
+
+    private void RecyclePrefabs()
+    {
+        for (int x = 0; x < InstantiatePrefabs.Length; x++)
+        {
+            GameObject prefab = InstantiatePrefabs[x];
+            prefab.transform.position = transform.position;
+        }
+    }
+}
