5095ed39123dd2fbdbeeaf1c42c772c4531028d6
Rodolfo <rodolfomsouza@gmail.com>
2019-11-18 22:43:18 -0300

    Tower toolbar improvement

---
+++ gameoff2019/Assets/RotateStar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateStar : MonoBehaviour
+{
+    Transform transform;
+    float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime *speed);
+    }
+}
