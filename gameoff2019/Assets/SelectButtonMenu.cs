f0157b4f9350423c8d013aec006e5379207e933e
Rodolfo <rodolfomsouza@gmail.com>
2019-11-19 12:36:29 -0300

    Update Menu

---
+++ gameoff2019/Assets/SelectButtonMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectButtonMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gema;
+    LoadGame loadGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gema.SetActive(false);
+        loadGame = FindObjectOfType<LoadGame>();
+    }
+
+    private void OnMouseDown()
+    {
+        switch (gameObject.name)
+        {
+            case "BtnPlay":
+                loadGame.Play();
+                break;
+            case "BtnContinue":
+                loadGame.Continue();
+                break;
+            case "BtnExit":
+                loadGame.Exit();
+                break;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        gema.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        gema.SetActive(false);
+    }
+}
