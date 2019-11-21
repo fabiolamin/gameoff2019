72a4a47a548d8a6d9009054dbf0db6254b1c1968
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-21 09:47:58 -0300

    Creating WallCollider script

---
+++ gameoff2019/Assets/Scripts/Wall/WallCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
