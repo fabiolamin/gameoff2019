2ff004f042b333c05ac4f36819984882185a42a8
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-23 09:13:52 -0300

    Creating Points script

---
+++ gameoff2019/Assets/Scripts/Points/Points.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    public int Value { get; private set; }
+
+    private void Awake()
+    {
+        Value = 0;
+    }
+    public void Change(int amount)
+    {
+        Value += amount;
+    }
+}
