59183659c4407d1619675d77600d7ce42352a47c
Rodolfo <rodolfomsouza@gmail.com>
2019-11-25 18:29:30 -0300

    Reload Stage after game over

---
+++ gameoff2019/Assets/Scripts/CanvasGameOver/CanvasGameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CanvasGameOver : MonoBehaviour
+{
+    [SerializeField]
+    GameObject loading;
+    public void RestartStage()
+    {
+        loading.SetActive(true);
+        Invoke("Reload", 3);
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
