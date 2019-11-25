016b8543233b7b0ed3f8cd979f4aedf651d26418
Rodolfo <rodolfomsouza@gmail.com>
2019-11-25 18:21:15 -0300

    Lives for the player

---
+++ gameoff2019/Assets/Scripts/ControlChips/ControlChips.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlChips : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] goChips;
+    [SerializeField]
+    private GameObject canvasGameOver;
+    private int chips;
+
+    private void Start()
+    {
+        chips = goChips.Length;
+        canvasGameOver.SetActive(false);
+    }
+
+    public void Damage()
+    {
+        if (chips > 0)
+        {
+            goChips[chips - 1].SetActive(false);
+            chips--;
+            if (chips <= 0)
+            {
+                canvasGameOver.SetActive(true);
+            }
+        }
+        
+    }
+}
--- gameoff2019/Assets/Scripts/Wall/WallCollider.cs
+++ gameoff2019/Assets/Scripts/Wall/WallCollider.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class WallCollider : MonoBehaviour
 {
+    ControlChips controlChips;
+    private void Start()
+    {
+        controlChips = FindObjectOfType<ControlChips>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
+            controlChips.Damage();
             GameObject enemy = other.gameObject;
             enemy.SetActive(false);
         }
