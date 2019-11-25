07dd0ae927a2ec77ae68a572d08e0df5438e316f
Rodolfo <rodolfomsouza@gmail.com>
2019-11-25 14:03:44 -0300

    Start Stage Button

---
+++ gameoff2019/Assets/Scripts/Stage/StartStage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartStage : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spawnEnimies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnEnimies.SetActive(false);
+    }
+
+    public void StartSpawnEnemies(GameObject btnStartStage)
+    {
+        btnStartStage.SetActive(false);
+        spawnEnimies.SetActive(true);
+    }
+}
