10894f63397b19612a80245cc367ed2ba462bd32
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:14:08 -0300

    Refactoring EnemiesBar script

---
+++ gameoff2019/Assets/Scripts/Enemy/EnemiesBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemiesBar : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slideBarEnemies;
+    private EnemySpawn enemySpawn;
+
+    void Update()
+    {
+        if (enemySpawn.gameObject != null && enemySpawn.gameObject.activeSelf)
+        {
+            slideBarEnemies.value = enemySpawn.position;
+        }
+    }
+
+    public void ChangeEnemiesSlideBar(GameObject enemySpawnGameObject) {
+        enemySpawn = enemySpawnGameObject.GetComponent<EnemySpawn>();
+        slideBarEnemies.maxValue = enemySpawn.enemyPool.GetInstances();
+        slideBarEnemies.value = enemySpawn.position;
+    }
+}
