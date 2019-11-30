f04594cd227fa616d4f4c6bf85e00692a65118f9
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:13:31 -0300

    Refactoring GameExit script

---
+++ gameoff2019/Assets/Scripts/GameManager/GameExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameExit : MonoBehaviour
+{
+    [SerializeField]
+    GameObject loading;
+    int indexScene;
+
+    private void Start()
+    {
+        loading.SetActive(false);
+        indexScene = SceneManager.GetActiveScene().buildIndex;
+    }
+    public void Exit()
+    {
+        loading.SetActive(true);
+        Invoke("LoadMenu", 3f);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(indexScene - 1);
+    }
+}
