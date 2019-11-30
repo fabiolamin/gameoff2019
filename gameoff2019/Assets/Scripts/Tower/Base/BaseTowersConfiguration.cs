b4c1b6759a8725d1e068e4a65fe6d4554b03a882
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:08:15 -0300

    Refactoring BaseTowersConfiguration script

---
+++ gameoff2019/Assets/Scripts/Tower/Base/BaseTowersConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTowersConfiguration : MonoBehaviour
+{
+    private GameObject[] bases;
+    private GameObject toolbarBaseTower;
+    private Animator animatorToolbarTower;
+    void Start()
+    {
+        PressetBaseTowers();
+    }
+
+    private void PressetBaseTowers()
+    {
+        HideTowersInBase();
+
+        toolbarBaseTower = GameObject.FindGameObjectWithTag("ToolbarBaseTower");
+        animatorToolbarTower = toolbarBaseTower.GetComponent<Animator>();
+        bases = GameObject.FindGameObjectsWithTag("BaseTower");
+        foreach (GameObject go in bases)
+        {
+            go.GetComponent<BaseSelection>().SetToolBarBase(toolbarBaseTower);
+        }
+
+    }
+
+    private static void HideTowersInBase()
+    {
+        GameObject[] towersInBase = GameObject.FindGameObjectsWithTag("Tower");
+        foreach (GameObject tb in towersInBase)
+        {
+            tb.SetActive(false);
+        }
+    }
+
+    public void CloseToolbarBase()
+    {
+        animatorToolbarTower.SetBool("isOpen", false);
+        animatorToolbarTower.SetBool("isClose", true);
+    }
+
+}
