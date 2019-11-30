76dfbafd1e69c6b3ef1893f1afff87ed49d72784
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:11:12 -0300

    Refactoring Trash script

---
+++ gameoff2019/Assets/Scripts/GameManager/Trash.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject[] trashGameObjects = GameObject.FindGameObjectsWithTag("Trash");
+        foreach(GameObject trashGameObject in trashGameObjects)
+        {
+            trashGameObject.transform.SetParent(gameObject.transform);
+        }
+    }
+}
