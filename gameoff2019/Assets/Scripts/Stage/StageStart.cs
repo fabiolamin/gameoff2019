a366d17230e945e5baafe2b5e11a2c11b444dc8b
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:10:03 -0300

    Refactoring StageStart script

---
+++ gameoff2019/Assets/Scripts/Stage/StageStart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageStart : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] spawnEnimies;
+    void Start()
+    {
+        int totalEnemies = 0;
+        spawnEnimies = GameObject.FindGameObjectsWithTag("EnemySpawn");
+        EnemiesBar enemiesBar = FindObjectOfType<EnemiesBar>();
+        enemiesBar.ChangeEnemiesSlideBar(spawnEnimies[0]);
+        foreach(GameObject spawnEnemy in spawnEnimies)
+        {
+            totalEnemies += spawnEnemy.GetComponent<EnemySpawn>().enemyPool.GetInstances();
+            spawnEnemy.SetActive(false);
+        }
+
+        FindObjectOfType<StageWin>().totalEnemies = totalEnemies;
+    }
+
+    public void ActivateSpawnEnemies(GameObject btnStartStage)
+    {
+        btnStartStage.SetActive(false);
+        foreach (GameObject spawnEnemy in spawnEnimies)
+        {
+            spawnEnemy.SetActive(true);
+        }
+    }
+}
