8d976aa8f07ec83afa4d59b3d4fad7eaa71e4e74
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:13:48 -0300

    Refactoring CanvasLock script

---
+++ gameoff2019/Assets/Scripts/GameManager/CanvasLock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasLock : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("Initiate", 3f);
+    }
+
+    public void Initiate()
+    {
+        gameObject.SetActive(false);
+    }
+}
