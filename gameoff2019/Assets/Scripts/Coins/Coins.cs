14b82ef56461b9cb80a9ae2f76d7dc59c5930c5a
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:14:26 -0300

    Refactoring Coins script

---
+++ gameoff2019/Assets/Scripts/Coins/Coins.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Coins : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI txtCoins;
+    private Player player;
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        UpdateCoins();
+    }
+
+    public void UpdateCoins()
+    {
+        txtCoins.text = player.Coins.ToString();
+    }
+
+
+
+}
