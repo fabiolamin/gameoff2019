caed11045090d49791335198f2eaba3fe0d7b307
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:11:25 -0300

    Refactoring StarRotation script

---
+++ gameoff2019/Assets/Scripts/GameManager/StarRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarRotation : MonoBehaviour
+{
+    private Transform transform;
+    float speed = 50f;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime *speed);
+    }
+}
