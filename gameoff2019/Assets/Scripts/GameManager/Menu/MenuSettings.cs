6298026f0f7c445583f02451c1982d9777d8b768
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:12:21 -0300

    Refactoring MenuSettings script

---
+++ gameoff2019/Assets/Scripts/GameManager/Menu/MenuSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSettings : MonoBehaviour
+{
+    [SerializeField] Slider sliderMusic;
+    [SerializeField] Slider sliderEffects;
+    [SerializeField] AudioSource musicScene;
+    [SerializeField] AudioSource[] effectScene;
+    public void Start()
+    {
+        sliderMusic.value = PlayerPrefs.GetFloat("VolumeMusic");
+        sliderEffects.value = PlayerPrefs.GetFloat("VolumeEffects");
+    }
+    public void ChangeMusic()
+    {
+        musicScene.volume = sliderMusic.value;
+        PlayerPrefs.SetFloat("VolumeMusic", sliderMusic.value);
+    }
+    public void ChangeEffect()
+    {
+        effectScene[0].volume = sliderEffects.value;
+        effectScene[1].volume = sliderEffects.value;
+        PlayerPrefs.SetFloat("VolumeEffects", sliderEffects.value);
+    }
+
+}
