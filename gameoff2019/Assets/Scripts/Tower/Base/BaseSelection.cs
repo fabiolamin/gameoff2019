90aa3ae57a3db68f8a0dc0f4886c0b76df922d64
Fábio Lamin <fabio_lamin@hotmail.com>
2019-11-30 19:08:37 -0300

    Refactoring BaseSelection script

---
+++ gameoff2019/Assets/Scripts/Tower/Base/BaseSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSelection : MonoBehaviour
+{
+    private Animator animator;
+    private ControlChangingTowers controlChangingTowers;
+
+    private void Start()
+    {
+        controlChangingTowers = FindObjectOfType<ControlChangingTowers>();
+    }
+
+    public void SetToolBarBase(GameObject baseToolBar)
+    {
+        animator = baseToolBar.GetComponent<Animator>();
+    }
+    public void Select()
+    {
+        controlChangingTowers.SetChangeTowers(GetComponent<TowerChange>());
+        animator.SetBool("isOpen", true);
+        animator.SetBool("isClose", false);
+    }
+}
